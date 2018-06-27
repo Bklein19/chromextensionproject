fb8296cf19b1b835e3a2fe4d88bf7751f4d46611
Kyle Stanfield <kylesonfon@gmail.com>
2018-06-27 14:18:51 -0700

    search the html and adjust popup.html

--- popup.js
+++ popup.js
@@ -1,7 +1,15 @@
 $.get('https://destiny.gg' , function(response) {
-	//do scraping
-	console.log(response);
-	console.log("hello world!");
+	
+	var position = response.search('"stream-status" class="online"');
+	if(position >= 0) {
+		document.getElementById("linkText").innerHTML = "Destiny is live!";	
+	}
+	else {
+		document.getElementById("linkText").innerHTML = "Destiny is offline.";
+	}
+
+	//console.log(response);
+	//console.log("hello world!");
 });
 	
 
